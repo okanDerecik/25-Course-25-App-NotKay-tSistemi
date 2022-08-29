ac3128d21dfe6d3e2c59835f3c68ec4b4781eeb8
okanDerecik <96899474+okanDerecik@users.noreply.github.com>
2022-08-30 00:18:11 +0300

    Add project files.

---
+++ Not_Kayit_Sistemi/FrmOgrenciDetay.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Not_Kayit_Sistemi
+{
+    partial class FrmOgrenciDetay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblAdSoyad = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LblSinav1 = new System.Windows.Forms.Label();
+            this.LblSinav2 = new System.Windows.Forms.Label();
+            this.LblSinav3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.LblNumara = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.LblOrtalama = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.LblDurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad Soyad :";
+            // 
+            // LblAdSoyad
+            // 
+            this.LblAdSoyad.AutoSize = true;
+            this.LblAdSoyad.Location = new System.Drawing.Point(104, 25);
+            this.LblAdSoyad.Name = "LblAdSoyad";
+            this.LblAdSoyad.Size = new System.Drawing.Size(52, 18);
+            this.LblAdSoyad.TabIndex = 1;
+            this.LblAdSoyad.Text = "NULL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sınav 1 :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sınav 2 :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Sınav 3 :";
+            // 
+            // LblSinav1
+            // 
+            this.LblSinav1.AutoSize = true;
+            this.LblSinav1.Location = new System.Drawing.Point(144, 116);
+            this.LblSinav1.Name = "LblSinav1";
+            this.LblSinav1.Size = new System.Drawing.Size(28, 18);
+            this.LblSinav1.TabIndex = 5;
+            this.LblSinav1.Text = "00";
+            // 
+            // LblSinav2
+            // 
+            this.LblSinav2.AutoSize = true;
+            this.LblSinav2.Location = new System.Drawing.Point(144, 147);
+            this.LblSinav2.Name = "LblSinav2";
+            this.LblSinav2.Size = new System.Drawing.Size(28, 18);
+            this.LblSinav2.TabIndex = 6;
+            this.LblSinav2.Text = "00";
+            // 
+            // LblSinav3
+            // 
+            this.LblSinav3.AutoSize = true;
+            this.LblSinav3.Location = new System.Drawing.Point(144, 179);
+            this.LblSinav3.Name = "LblSinav3";
+            this.LblSinav3.Size = new System.Drawing.Size(28, 18);
+            this.LblSinav3.TabIndex = 7;
+            this.LblSinav3.Text = "00";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(20, 54);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 18);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Numara :";
+            // 
+            // LblNumara
+            // 
+            this.LblNumara.AutoSize = true;
+            this.LblNumara.Location = new System.Drawing.Point(104, 54);
+            this.LblNumara.Name = "LblNumara";
+            this.LblNumara.Size = new System.Drawing.Size(48, 18);
+            this.LblNumara.TabIndex = 9;
+            this.LblNumara.Text = "0000";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(38, 215);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(84, 18);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Ortalama :";
+            // 
+            // LblOrtalama
+            // 
+            this.LblOrtalama.AutoSize = true;
+            this.LblOrtalama.Location = new System.Drawing.Point(144, 215);
+            this.LblOrtalama.Name = "LblOrtalama";
+            this.LblOrtalama.Size = new System.Drawing.Size(28, 18);
+            this.LblOrtalama.TabIndex = 11;
+            this.LblOrtalama.Text = "00";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(55, 249);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(67, 18);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Durum :";
+            // 
+            // LblDurum
+            // 
+            this.LblDurum.AutoSize = true;
+            this.LblDurum.Location = new System.Drawing.Point(131, 249);
+            this.LblDurum.Name = "LblDurum";
+            this.LblDurum.Size = new System.Drawing.Size(52, 18);
+            this.LblDurum.TabIndex = 13;
+            this.LblDurum.Text = "NULL";
+            // 
+            // FrmOgrenciDetay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(339, 411);
+            this.Controls.Add(this.LblDurum);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.LblOrtalama);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.LblNumara);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.LblSinav3);
+            this.Controls.Add(this.LblSinav2);
+            this.Controls.Add(this.LblSinav1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LblAdSoyad);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmOgrenciDetay";
+            this.Text = "Öğrenci Not Kayıt Sistemi";
+            this.Load += new System.EventHandler(this.FrmOgrenciDetay_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblAdSoyad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label LblSinav1;
+        private System.Windows.Forms.Label LblSinav2;
+        private System.Windows.Forms.Label LblSinav3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label LblNumara;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label LblOrtalama;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label LblDurum;
+    }
+}
---
+++ Not_Kayit_Sistemi/FrmOgretmenDetay.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Not_Kayit_Sistemi
+{
+    public partial class FrmOgretmenDetay : Form
+    {
+        public FrmOgretmenDetay()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-K0C08G8;Initial Catalog=DbNotKayit;Integrated Security=True");
+
+        private void FrmOgretmenDetay_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dbNotKayitDataSet.TBLDERS' table. You can move, or remove it, as needed.
+            this.tBLDERSTableAdapter.Fill(this.dbNotKayitDataSet.TBLDERS);
+
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("Select Count(*) from TBLDERS where DURUM='true'",baglanti);
+            SqlDataReader dr = komut2.ExecuteReader();
+            while (dr.Read())
+            {
+                LblGecenSayisi.Text = dr[0].ToString();
+            }
+            baglanti.Close();
+
+            baglanti.Open();
+            SqlCommand komut3 = new SqlCommand("Select Count(*) from TBLDERS where DURUM='false'", baglanti);
+            SqlDataReader dr2 = komut3.ExecuteReader();
+            while (dr2.Read())
+            {
+                LblKalanSayisi.Text = dr2[0].ToString();
+            }
+            baglanti.Close();
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into TBLDERS (OGRNUMARA,OGRAD,OGRSOYAD) VALUES (@P1,@P2,@P3)",baglanti);
+            komut.Parameters.AddWithValue("@P1", MskNumara.Text);
+            komut.Parameters.AddWithValue("@P2", TxtAd.Text);
+            komut.Parameters.AddWithValue("@P3", TxtSoyad.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Öğrenci Sisteme Eklendi");
+            this.tBLDERSTableAdapter.Fill(this.dbNotKayitDataSet.TBLDERS);
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+
+            MskNumara.Text = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+            TxtAd.Text = dataGridView1.Rows[secilen].Cells[2].Value.ToString();
+            TxtSoyad.Text = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            TxtSinav1.Text = dataGridView1.Rows[secilen].Cells[4].Value.ToString();
+            TxtSinav2.Text = dataGridView1.Rows[secilen].Cells[5].Value.ToString();
+            TxtSinav3.Text = dataGridView1.Rows[secilen].Cells[6].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double ortalama, s1, s2, s3;
+            string durum;
+            s1 = Convert.ToDouble(TxtSinav1.Text);
+            s2 = Convert.ToDouble(TxtSinav2.Text);
+            s3 = Convert.ToDouble(TxtSinav3.Text);
+            ortalama = (s1 + s2 + s3) / 3;
+            LblOrtalama.Text = ortalama.ToString();
+
+            if(ortalama >= 50)
+            {
+                durum = "True";
+            }
+            else
+            {
+                durum = "False";
+            }
+
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Update TBLDERS set OGRS1=@P1,OGRS2=@P2,OGRS3=@P3,ORTALAMA=@P4,DURUM=@P5 where OGRNUMARA = @P6", baglanti);
+            komut.Parameters.AddWithValue("@P1", TxtSinav1.Text);
+            komut.Parameters.AddWithValue("@P2", TxtSinav2.Text);
+            komut.Parameters.AddWithValue("@P3", TxtSinav3.Text);
+            komut.Parameters.AddWithValue("@P4", decimal.Parse(LblOrtalama.Text));
+            komut.Parameters.AddWithValue("@P5", durum);
+            komut.Parameters.AddWithValue("@P6", MskNumara.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Öğrenci Notları Güncellendi");
+            this.tBLDERSTableAdapter.Fill(this.dbNotKayitDataSet.TBLDERS);
+
+
+        }
+    }
+}
